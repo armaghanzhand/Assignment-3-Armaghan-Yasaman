30a6e643a87451c82b344c0e3b6cae895402ce12
Yasaman <73587697+yasamaneskandari1994@users.noreply.github.com>
2021-01-25 22:56:02 +0330

    Finalizing Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
